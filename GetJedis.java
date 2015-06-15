f52a8d8e33e77faba4218211b0c516079fd2707b
weiguoyuan <weiguoyuan@163.com>
2015-06-15 16:56:54 +0800

    GetJedis.java
    get Redis connection

---
+++ GetJedis.java
@@ -0,0 +1,13 @@
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPoolConfig;
+
+import com.wandoulabs.jodis.JedisResourcePool;
+import com.wandoulabs.jodis.RoundRobinJedisPool;
+
+public class GetJedis{
+	private static JedisResourcePool jedisPool = new RoundRobinJedisPool("10.64.4.57:2181,192.10.64.4.95:2181,192.10.64.4.99:2181",
+			30000, "/zk/codis/db_test/proxy", new JedisPoolConfig());
+	public static Jedis getJedis(){
+		return jedisPool.getResource();
+	}
+}
