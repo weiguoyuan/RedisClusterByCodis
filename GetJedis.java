14eeff1ddddfa053db1b94be308713393411a332
weiguoyuan <weiguoyuan@163.com>
2015-06-16 10:03:40 +0800

    Update GetJedis.java

--- GetJedis.java
+++ GetJedis.java
@@ -6,7 +6,7 @@ import com.wandoulabs.jodis.RoundRobinJedisPool;
 
 public class GetJedis{
 	private static JedisResourcePool jedisPool = new RoundRobinJedisPool("10.64.4.57:2181,192.10.64.4.95:2181,192.10.64.4.99:2181",
-			30000, "/zk/codis/db_test/proxy", new JedisPoolConfig());
+			30000, "/zk/codis/db_test/proxy", new JedisPoolConfig());//第一个参数为zookeeper节点列表
 	public static Jedis getJedis(){
 		return jedisPool.getResource();
 	}
